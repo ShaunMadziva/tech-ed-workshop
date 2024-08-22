c6e6a2fb2e1392499e29ecaa591959f692b0c546
Shaun <shaunmadziva@gmail.com>
2024-08-22 10:53:58 +0100

    for loops

--- week02/arrays-and-loops/loops.js
+++ week02/arrays-and-loops/loops.js
@@ -6,15 +6,33 @@ while (counter < 10) {
   counter++;
 }
 
-const answer = 9;
-let guess = prompt("Guess the number");
-counter = 0;
-while (guess != answer && counter < 10) {
-  alert("Not correct");
-  counter++;
-  guess = prompt(`Guess again you have ${10 - counter} chances left`);
+// const answer = 9;
+// let guess = prompt("Guess the number");
+// counter = 0;
+// while (guess != answer && counter < 10) {
+//   alert("Not correct");
+//   counter++;
+//   guess = prompt(`Guess again you have ${10 - counter} chances left`);
+// }
+
+// alert("Correct");
+
+//for loops are best when you know how many iterations you want to run the loop for.
+
+//for (set intial value of counter; a condition the counter needs to meet; update counter after each iteration of code) {so code to run}
+
+const foods = ["pizza", "chocolate", "ice cream", "cheese"];
+for (let counter = 0; counter < foods.length; counter++) {
+  console.log(counter);
+  console.log(foods[counter]);
 }
 
-alert("Correct");
+// for...of
+for (let food of foods) {
+    console.log(food, "for...of");
+  }
 
-//for loops are best when you know how many iterations you want to run the loop for
+  // forEach
+  foods.forEach(function (food) {
+    console.log(food, "forEach");
+  });
