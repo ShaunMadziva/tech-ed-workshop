1b012e89042a8ef41b44eab37134e572986e865a
Shaun <shaunmadziva@gmail.com>
2024-09-04 16:00:15 +0100

    () => {}

---
+++ week04/arrow-funtions/app.js
@@ -0,0 +1,12 @@
+//normal function
+function sum(a, b) {
+  return a + b;
+}
+
+//arrow function
+const sum2 = (a, b) => {
+  return a + b;
+};
+
+// with arrow functions you can write everting in one line and the return is implied.
+const onelineSum = (a, b) => a + b; //this will return the sum of a+b.
