1c95dab186da6c8de14ea45aafead5a8bb5ec548
Shaun <shaunmadziva@gmail.com>
2024-09-16 12:36:01 +0100

    save

--- week06/react-app/react-project/src/App.jsx
+++ week06/react-app/react-project/src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
+  
   return (
     <>
-     <h2>This is a Raect app, yay!</h2>
-     <p>this is a p tag!! </p>
+      <h2>This is a Raect app, yay!</h2>
+      <p>this is a p tag!! </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
