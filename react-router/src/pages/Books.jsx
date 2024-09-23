5966b01c5dcc76b7d5c333677a60c021bb553a72
Shaun <shaunmadziva@gmail.com>
2024-09-23 10:45:57 +0100

    react router

---
+++ react-router/src/pages/Books.jsx
@@ -0,0 +1,15 @@
+export default function Books() {
+  return (
+    <div>
+      <h2>These are my books of the month</h2>
+      <div>
+        <img
+          src="https://m.media-amazon.com/images/I/71jaSadBJvL._AC_UF894,1000_QL80_.jpg"
+          alt="book cover"
+        />
+        <p>Naughts & Crosses</p>
+        <p>Malorie Blackman</p>
+      </div>
+    </div>
+  );
+}
