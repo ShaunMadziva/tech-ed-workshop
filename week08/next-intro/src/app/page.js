f3deae04503905a7c4601867594e3e06eda763ea
Shaun <shaunmadziva@gmail.com>
2024-10-02 14:17:53 +0100

    next Image tag

--- week08/next-intro/src/app/page.js
+++ week08/next-intro/src/app/page.js
@@ -1,6 +1,10 @@
+import Image from "next/image";
+import myImage from "@/../public/myImage.png";
+
 export default function Home() {
   return (
     <div>
+       <Image alt="two beatles" src={myImage} />
       <p>Home Page</p>
     </div>
   )
