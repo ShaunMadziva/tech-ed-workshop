7de43084b06d946ec1fd9626aecb41b0df6bac0b
Shaun <shaunmadziva@gmail.com>
2024-10-03 11:38:18 +0100

    animation with framer-motion

--- week08/postgres-next/src/app/header.js
+++ week08/postgres-next/src/app/header.js
@@ -1,15 +1,26 @@
-import Link from "next/link"
+"use client";
+
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Header() {
-    return (
-        <div>
-            <nav>
-                <ul>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-    )
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+        </ul>
+      </nav>
+      <motion.div
+        initial={{ opacity: 0, scale: 0.2 }}
+        whileInView={{ opacity: 1, scale: 1 }}
+        viewport={{ once: false }}
+        transition={{ duration: 1 }}
+      >
+        <h1>Hello world</h1>
+      </motion.div>
+    </div>
+  );
 }
